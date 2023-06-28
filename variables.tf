56abb94b807ed98131125c67419a0e0a9133cb42
Vijay-bootlabs <vijay@bootlabstech.com>
2023-06-28 16:46:18 +0530

    initial commit

--- variables.tf
+++ variables.tf
@@ -66,6 +66,6 @@ variable "encryption_resources" {
 }
 
 variable "arn" {
-  type        = map(string)
+  type        = string
   description = "(Required) ARN of the Key Management Service (KMS) customer master key (CMK). The CMK must be symmetric, created in the same region as the cluster, and if the CMK was created in a different account, the user must have access to the CMK."
 }
